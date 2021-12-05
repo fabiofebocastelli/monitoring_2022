f1541af026c118f3e47d7adf2760735e3cc9fc79
fabiofebocastelli <94614136+fabiofebocastelli@users.noreply.github.com>
2021-12-05 16:56:12 +0100

    Update R_code_quantitative_estimates_land_cover.r

--- R_code_quantitative_estimates_land_cover.r
+++ R_code_quantitative_estimates_land_cover.r
@@ -22,4 +22,33 @@ plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
 l2006 <- listrast[[2]]
 plotRGB(l2006, r=1, g=2, b=3, stretch="lin")
 
+#unsupervised classification 
+library(RStoolbox) # classification
+l1992c <- unsuperClass(l1992, nClasse=2)
+plot(l1992c$map)
+# value 1 = is forest and value 2= water and agriculture
+#new let's check the frequencies of the pixels.. how many pixels inside my maps are forest? and how many are field crops?
+# use freq function 
+freq(l1992c$map)
+total <-  341292 #these are the total amount of pixels in l1992 file
+# we want to check the proportion..
+propforest <-  308561/total
+propagri <- 32731/total
+propforest
+propagri
 
+#build a dataframe 
+cover <- c("Forest", "Agriculture") # this is an array!
+prop1992 <- c(0.9040968, 0.09590321)
+
+proportion1992 <- data.frame(cover, prop1992)
+
+#plot the data with ggplot2
+library(ggplot2)
+
+ggplot(proportion1992, aes(x=cover, y= prop1992, color= cover))
+#we want to have an histogram: geom_bar function 
+ggplot(proportion1992, aes(x=cover, y= prop1992, color= cover)) + geom_bar(stat="identity", fill="white") #identity means we want to use the values as they are
+#fill is for chosing the colour inside the bars of the histogram
+
+       
