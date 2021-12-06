6483d93aadfe55c5e329066d8b6277edb127be5c
fabiofebocastelli <94614136+fabiofebocastelli@users.noreply.github.com>
2021-12-06 10:11:20 +0100

    Update R_code_quantitative_estimates_land_cover.r

--- R_code_quantitative_estimates_land_cover.r
+++ R_code_quantitative_estimates_land_cover.r
@@ -51,4 +51,31 @@ ggplot(proportion1992, aes(x=cover, y= prop1992, color= cover))
 ggplot(proportion1992, aes(x=cover, y= prop1992, color= cover)) + geom_bar(stat="identity", fill="white") #identity means we want to use the values as they are
 #fill is for chosing the colour inside the bars of the histogram
 
-       
+ ## day 2 ##
+#mi si è bloccato il pc..
+recall all the past things .
+library(raster)
+library(RStoolbox) 
+library(ggplot2)
+
+# setwd("C:/lab/") # Windows
+
+# 1 list the files available
+
+rlist <- list.files(pattern="defor")
+
+rlist
+list_rast <- lapply(rlist, brick) # lapply(x, FUN)
+
+list_rast
+l1992 <- list_rast[[1]]
+plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
+
+# unsupervised classification:  unsuperClass function 
+#we shift from a rasterbrick to a single raster layer
+l1992c <- unsuperClass(l1992, nClasse=2)
+# no more continuous values (0-255) but only 2 values (1 or 2)
+#let's plot the map
+plot(l1992c$map)
+#let's compute the frequencies 
+freq(l1992c$map)
