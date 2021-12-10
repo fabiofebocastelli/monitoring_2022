22fea0d0d4f5f74549ea68008dd01ee0940f159e
fabiofebocastelli <94614136+fabiofebocastelli@users.noreply.github.com>
2021-12-10 13:58:28 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -33,7 +33,37 @@ plotRGB(l2011, r=3, g=2, b=1, stretch="Lin") # we have 3 channels: red, green an
 setwd("C:/lab/") #setting the wd for windows..
 
 l2011 <- brick("p224r63_2011_.grd") #assign the function to an object
-l2011 #you habve all the infos, you have several bands (layers). The image is called landsat (7 levels), each leyers we are recording the reflectance of each object
+l2011 #you have all the infos, you have several bands (layers). The image is called landsat (7 levels), each leyers we are recording the reflectance of each object
+#how to plot every single band:
+
+#let's plot only the green band. what's the name of the band n. 2?  B2_sre (spectral reflectance) ehich is inside the object "l2001"
+plot(l2011$B2_sre) # $ is for linking. plot is the function, inside the () is the argument. Pay attention to the capital letters! 
+#you get an image, but the color are meaningless..
+
+cl <- colorRampPalette(c("black","grey","light grey"))(100) # we can change the colours
+plot(l2011$B2_sre, col=cl) # now we get the new image with the color we chose 8from black to light grey)
+
+# EXERCISE: change the colorRampPalette with dark green, green, and light green, e.g. clg 
+clg <- colorRampPalette(c("dark green","green","light green"))(100)
+plot(l2011$B2_sre, col=clg)
+
+# do the same for the blue band using "dark blue", "blue", and "light blue"
+# B1
+clb <- colorRampPalette(c("dark blue","blue","light blue"))(100)
+plot(l2011$B1_sre, col=clb) # in this case we use B1_sre, the band of blue wavelength
+
+# plot both images in just one multiframe graph
+# par function: can be used to set or query graphical parameters.
+par(mfrow=c(1,2)) # the first number is the number of rows in the multiframe, while the second one is the number of columns
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
+
+# exercise: plot both images in just one multiframe graph with two rows and just one column
+par(mfrow=c(2,1))
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
+
+
 
 par(mfrow=c(2,2))
 
