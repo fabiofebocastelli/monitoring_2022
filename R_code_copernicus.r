ce042cd425b84ed4ad3fd53eaab7f930c6a0da12
fabiofebocastelli <94614136+fabiofebocastelli@users.noreply.github.com>
2021-12-18 15:49:52 +0100

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -10,7 +10,7 @@ NDVI20210110 <- raster("c_gls_NDVI300_202101010000_GLOBE_PROBAV_V1.0.1(2).nc") #
 NDVI20210110 #the number of pixels is huge: in my case is 5.689.958.400 
 plot(NDVI20210110)
 
-## DAY2 ##
+## DAY 2 ##
 
 # we continue from where we left..
 
@@ -45,6 +45,29 @@ NDVIstack
 NDVI2020 <- NDVIstack$Normalized.Difference.Vegetation.Index.333M.1
 NDVI2021 <- NDVIstack$Normalized.Difference.Vegetation.Index.333M.2
 
-#in this case it's not that usefull to stack and than unstack the files.. in fact it's just an exemples to let us know what to do in case of several files!
+# in this case it was not that usefull to stack and than unstack the files.. in fact it's just an exemples to let us know what to do in case of several files (images)!
+
+#patchwork packages:
+
+library(patchwork)
+
+p1 <- ggplot() + geom_raster(NDVI2020, mapping= aes(x=x, y=y, fill=Normalized.Difference.Vegetation.Index.333M.1)) + scale_fill_viridis() + ggtitle("NDVI in 2020")
+
+p2 <- ggplot() + geom_raster(NDVI2021, mapping= aes(x=x, y=y, fill=Normalized.Difference.Vegetation.Index.333M.2)) + scale_fill_viridis() + ggtitle("NDVI in 2021")
+
+#now let's patchwork them together, first of all assign a name to each plot:
+
+p1/p2
+
+# to ZOOM on a certain part of the image, use the crop function (coordinates):
+# es. longitude from 10 to 30, latitude from 35 to 55
+ 
+ext <- c(10, 30, 35, 55)
+NDVI2020_cropped <- crop(NDVI2020, ext)
+NDVI2021_cropped <- crop(NDVI2021, ext)
+
+p1 <- ggplot() + geom_raster(NDVI2020_cropped, mapping= aes(x=x, y=y, fill=Normalized.Difference.Vegetation.Index.333M.1)) + scale_fill_viridis() + ggtitle("NDVI in 2020")
+p2 <- ggplot() + geom_raster(NDVI2021_cropped, mapping= aes(x=x, y=y, fill=Normalized.Difference.Vegetation.Index.333M.2)) + scale_fill_viridis() + ggtitle("NDVI in 2021")
+p1/p2
 
 
