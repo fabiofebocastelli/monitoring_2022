078fe87f323a5d138cc5b154075a9995d7983d2c
fabiofebocastelli <94614136+fabiofebocastelli@users.noreply.github.com>
2021-12-29 19:17:25 +0100

    Update R_code_greenland.r

--- R_code_greenland.r
+++ R_code_greenland.r
@@ -1,24 +1,25 @@
-# Ice melt in Greenland
-# proxy: LST
+# Ice melting in Greenland # Time series analysis # Greenland increase of temperature
+
+# proxy: LST (land surface temperature)
 setwd("C:/lab/greenland")
 
 # reupload the data inside R:
-# list f files:
-rlist <- list.files(pattern="lst") #the pattern is the common part of the names
+# list files:
+rlist <- list.files(pattern="lst") # the pattern is the common part of the names of the files we want to work with
 rlist
 library(raster)
 import <- lapply(rlist,raster) #the files are single layers so we can use raster function
-import
+import # distinct infos on the files 
 tgr <- stack(import) #TGr= temperature of greenland
-tgr
-plot(tgr)
+tgr # joined infos on the files
+plot(tgr) # plot of the 4 images
 
 library(ggplot2)
 library(RStoolbox)
 library(patchwork)
 library(viridis)
 
-# ggplot of first and dinale images 2000 vs 2015:
+# ggplot of first and last images (2000 vs 2015):
 ggplot() + 
 geom_raster(tgr$lst_2000, mapping = aes(x=x, y=y, fill=lst_2000)) + 
 scale_fill_viridis(option="magma") +
@@ -29,7 +30,7 @@ geom_raster(tgr$lst_2015, mapping = aes(x=x, y=y, fill=lst_2015)) +
 scale_fill_viridis(option="magma") +
 ggtitle("LST in 2015")
 
-#now let's plot them together:
+# now let's plot them together:
 
 p1 <- ggplot() + 
 geom_raster(tgr$lst_2000, mapping = aes(x=x, y=y, fill=lst_2000)) + 
@@ -42,29 +43,29 @@ scale_fill_viridis(option="magma") +
 ggtitle("LST in 2015")
 
 p1/p2
-#or
+# or
 p1 + p2
 
-#plotting freq. distributions:
+# plotting frequencies distributions:
 hist(tgr$lst_2000)
 hist(tgr$lst_2015)
-#then 2 plot together: 
+# then 2 plot together: 
 par(mfrow=c(1,2))
 hist(tgr$lst_2000)
 hist(tgr$lst_2015)
 
-#then altogether:
+# then altogether:
 par(mfrow=c(2,2))
 hist(tgr$lst_2000)
 hist(tgr$lst_2005)
 hist(tgr$lst_2010)
 hist(tgr$lst_2015)
 
-# plotting two values on a piano cartesiano in order to have the values and a curve: abline function (N.B. Y= mX + c)
-dev.off()
-plot(tgr$lst_2010, tgr$lst_2015)
-abline(0,1,col="red")
-# to see the red line, change the values..
+# plotting two values on a cartesian plane with the values and also a curve: abline function (N.B. Y= mX + c)
+dev.off() # to remove the previous plot
+plot(tgr$lst_2010, tgr$lst_2015) # to get the cartesian plane with the values
+abline(0,1,col="red") # to get the red curve
+# to choose the X and Y axis range:
 plot(tgr$lst_2010, tgr$lst_2015, xlim=c(12500,15000), ylim=c(12500,15000))
 abline(0,1,col="red")
 
