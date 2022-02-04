2a28a8f5c3badf1ef000a20236a5b42652547edb
fabiofebocastelli <94614136+fabiofebocastelli@users.noreply.github.com>
2022-02-04 14:15:13 +0100

    Update R_code_exam_project.r

--- R_code_exam_project.r
+++ R_code_exam_project.r
@@ -6,6 +6,7 @@ install.packages('RStoolbox')
 install.packages('ggplot2')
 install.packages('rgdal')
 install.packages('sp')
+install.packages('mapview')
 
 library(raster) 
 library(RStoolbox) 
@@ -70,11 +71,11 @@ plot(clc_forest18)
 # prendo consigli da: https://gis.stackexchange.com/questions/422711/how-to-get-the-number-of-pixel-with-a-given-value-from-a-rasterlayer-in-r/422713#422713
 forest_cover18 <- sum(values(clc_forest18), na.rm=TRUE)
 [1] 34574143 # è minore del 2018
-forest_cover00 - forest_cover18
+diff_forest_cover <- forest_cover00 - forest_cover18
 [1] 61190 # c'è stata una diminuzione della copertura forestale complessiva di 61190 pixel, ovvero del 1.8 % 
 # la risoluzione è di 100 m (https://sdi.eea.europa.eu/catalogue/srv/eng/catalog.search#/metadata/5a5f43ca-1447-4ed0-b0a6-4bd2e17e4f4d)
 10000*61190
-[1] 611900000 # sono i mq persi di superfice boschiva. 61190 ettari
+[1] 6119000 # sono i mq complessivbi persi di superfice boschiva. 6,2 kmq (il centro di bologna è 4,5 kmq), 612 ettari
 
  # cerco di ottenere u grafico con gli istogrammi 
 reference_years <- c("2000", "2018")
@@ -85,26 +86,46 @@ forest_cover_change
 1            2000      34635333
 2            2018      34574143
 
-ggplot(forest_change, aes(x=reference_years, y= number_of_pixels, color= reference_years)) + geom_bar(stat="identity", fill="white") + ggtitle ("Corine Land Cover Accounting Layers: 1.8 % of forest cover reduction between 2000 and 2018") 
-# ottento gli istogrammi dei due anni 
+# ottengo gli istogrammi dei due anni:
+ggplot(forest_change, aes(x=reference_years, y= number_of_pixels, color= reference_years)) + geom_bar(stat="identity", fill="white") 
++ ggtitle ("Corine Land Cover Accounting Layers: 1.8 % of forest cover reduction between 2000 and 2018") 
 
-# metto i due grafici insieme con parfunction 
+fdiff = clc_forest18 - clc_forest00
+fdiff
+class      : RasterLayer 
+dimensions : 16000, 12000, 1.92e+08  (nrow, ncol, ncell)
+resolution : 100, 100  (x, y)
+extent     : 4e+06, 5200000, 1200000, 2800000  (xmin, xmax, ymin, ymax)
+crs        : +proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +units=m +no_defs 
+source     : r_tmp_2022-02-04_121500_4288_88587.grd 
+names      : layer 
+values     : -1, 1  (min, max)
+
+table(values(fdiff))
+
+       -1         0         1 
+   184463 191692264    123273   # 123273 è il numero di pixels in cui è avvenuto rimboschimento: 12,3 Kmq. 1.233 ettari
+
+
+# metto i due grafici insieme con par function 
 
 par(mfrow=c(1,2))
 plot(clc_forest00)
 plot(clc_forest18)
 
+# faccio plot della differenza 
+plot(fdiff)
 
-# provo a fare la sottrazione dei due grafici con Overlay Function:
-forest_cover_diff <- overlay(clc_forest18, clc_forest00, fun=function(r1, r2){return(r1-r2)})
-plot(forest_cover_diff, main="Difference between 2018 and 2000 a.k.a. Where forest came back")
+# suddivido l'immagine in 3 sottoimmagini per aumentare la risoluzione
+ext <- c(4e+06, 5200000, 2250000, 2800000)
+diff_north <- crop(fdiff, ext)
+cl <- colorRampPalette(c("red","white","blue"))(100)
+plot(diff_north, col=cl)
 
-forest_cover_diff <- overlay(clc_forest18, clc_forest00, fun=function(r1, r2){return(r2-r1)})
-plot(forest_cover_diff, main="Difference between 2018 and 2000 a.k.a. Where forest came back")
 
-#entrambi danno un immagine tutta gialla.. provo in un altro modo: 
-
-forest_cover_diff <- clc_forest18 - clc_forest00 
-plot(forest_cover_diff, main="Difference between 2018 and 2000 a.k.a. Where forest came back", axes=FALSE) 
+#provo ad importare limmagine con i confine dell europa:
+eu_coastline <- raster("Europe_coastline.shp") # non va
+eu_coastline <- raster("Europe_coastline_poly.shp")
+eu_coastline <- raster("HYP_50M_SR_W.tif")
 
 
